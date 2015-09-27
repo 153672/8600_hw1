aec20f96809cb8e9aca0dd390ad6826b42ce8323
Mart Aarma <mart.aarma@gmail.com>
2015-09-27 19:29:24 +0300

    solution

--- magichexagon/search_problem.py
+++ magichexagon/search_problem.py
@@ -1,57 +1,69 @@
 import search
 import time
+import random
 
 __author__ = 'Mart Aarma'
 
 class MagicHexagonProblem(search.Problem):
 
     def __init__(self):
-        self.initial = [0] * 19
-        self.initial[0] = 1
 
+        # idx 0-> /a|b|c\
+        #        /d|e|f|g\
+         #      /h|i|j|k|l\
+        #        \m|n|o|p/
+        #         \r|s|t/
+
+        # idx 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+        #     a,b,c,g,l,p,t,s,r,m,h, d, e, f, k, o, n, i, j
+
+        # Before going further to inner circle the outer rows of 3 must be correct
+
+        self.initial = tuple([0] * 19)
+        self.idx = 0;
+        self.c = 0
+
+    # tagastab v6imalikud v22rtused indeksi jaoks ja otsustab milline on j2rmine indeks
     def actions(self, state):
+
+        if state[0] == 0:
+            return [e for e in reversed(range(1,20))]
+
         if self.partialSolutionIsValid(state):
-            return ["startNext"]
+            e = [e for e in reversed(range(1,20)) if e not in state]
+            return e
         else:
-            if self.valueIsMaxAvailValue(state, state[state.index(0) - 1]):
-                return ["incPrevVal"]
+            if state[-1] == 0:
+                idx = state.index(0) - 1
             else:
-                return ["incCurrVal"]
+                idx = 18
+
+            if idx in (3,5,7,9,11) and not self.checkRowOf3(state, idx):
+                return []
+
+            v = state[idx];
+            e = [e for e in reversed(range(1,20)) if e > v and e not in state]
+            return e
 
     def result(self, state, action):
-        index = state.index(0)
-        currentIndex = index - 1;
-        if action == "startNext":
-            state[state.index(0)] = self.findNextElement(state)
-        elif action == "incCurrVal":
-            state[currentIndex] = self.findNextBiggerValue(state, state[currentIndex])
+        localstate = list(state)
+        if action == 0:
+            print("!!!!!!!!!!!!!")
+            localstate[state.index(0) - 1] = 0
         else:
-            self.incPrevValue(state, currentIndex)
-        return state
+            localstate[state.index(0)] = action
 
-    def incPrevValue(self, state, index):
-        state[index] = 0
-        elem = state[index - 1]
-        newValue = self.findNextBiggerValue(state, elem)
-        if elem == newValue:
-            self.incPrevValue(state, index - 1)
-        else:
-            state[index - 1] = newValue
+        #print("result:", localstate)
+        return tuple(localstate)
 
     def valueIsMaxAvailValue(self, state, value):
-        for e in range(value + 1, 20):
-            if e not in state:
-                return False
-        return True
+         for e in range(1, 20):
+             if e not in state and (e > value):
+                 return False
+         return True
 
     def findNextBiggerValue(self, state, min):
         for e in range(min, 20):
-            if e not in state:
-                return e
-        return min
-
-    def findNextElement(self, state):
-        for e in range(1, 20):
             if e not in state:
                 return e
         return min
@@ -64,14 +76,7 @@ class MagicHexagonProblem(search.Problem):
 
     def checkSolution(self, state, completeCheck):
 
-        if completeCheck and state[-1] == 0:
-            return False
-
-        #        /a|b|c\
-        #       /d|e|f|g\
-        #      /h|i|j|k|l\
-        #       \m|n|o|p/
-        #        \r|s|t/
+        state = [18, 17, 3, 19, 16, 12, 10, 13, 15, 14, 9, 11, 1, 7, 2, 4, 8, 6, 5]
 
         a = state[0]
         b = state[1]
@@ -93,25 +98,35 @@ class MagicHexagonProblem(search.Problem):
         i = state[17]
         j = state[18]
 
-        # 3 17 18 11 9 14 15 13 10 12 16 19 7 1 6 8 4 2 5
+        completeCheck = False
+        rows3Complete = False;
 
-        if self.checkRowsOf3(a, b, c, completeCheck):
-            if self.checkRowsOf3(c, g, l, completeCheck):
-                if self.checkRowsOf3(l, p, t, completeCheck):
-                    if self.checkRowsOf3(t, s, r, completeCheck):
-                        if self.checkRowsOf3(r, m, h, completeCheck):
-                            if self.checkRowsOf3(h, d, a, completeCheck):
-                                if self.checkRowsOf4(d, e, f, g, completeCheck):
-                                    if self.checkRowsOf4(g, k, o, s, completeCheck):
-                                        if self.checkRowsOf4(b, f, k, p, completeCheck):
-                                            if self.checkRowsOf4(m, n, o, p, completeCheck):
-                                                if self.checkRowsOf4(b, e, i, m, completeCheck):
-                                                    if self.checkRowsOf4(d, i, n, s, completeCheck):
-                                                        if self.checkRowsOf5(c, f, j, n, r, completeCheck):
-                                                            if self.checkRowsOf5(h, i, j, k, l, completeCheck):
-                                                                if self.checkRowsOf5(a, e, j, o, t, completeCheck):
-                                                                    return True
+        if not completeCheck:
+            if self.checkRowsOf3(a, b, c, completeCheck):
+                if self.checkRowsOf3(c, g, l, completeCheck):
+                    if self.checkRowsOf3(l, p, t, completeCheck):
+                        if self.checkRowsOf3(t, s, r, completeCheck):
+                            if self.checkRowsOf3(r, m, h, completeCheck):
+                                if self.checkRowsOf3(h, d, a, completeCheck):
+                                    rows3Complete = True
 
+        if completeCheck or rows3Complete:
+            if self.checkRowsOf4(d, e, f, g, completeCheck):
+                if self.checkRowsOf4(g, k, o, s, completeCheck):
+                    if self.checkRowsOf4(b, f, k, p, completeCheck):
+                        if self.checkRowsOf4(m, n, o, p, completeCheck):
+                            if self.checkRowsOf4(b, e, i, m, completeCheck):
+                                if self.checkRowsOf4(d, i, n, s, completeCheck):
+                                    if self.checkRowsOf5(c, f, j, n, r, completeCheck):
+                                        if self.checkRowsOf5(h, i, j, k, l, completeCheck):
+                                            if self.checkRowsOf5(a, e, j, o, t, completeCheck):
+                                                return True
+
+        return False
+
+    def checkRowOf3(self, state, index):
+        if state[index-3] + state[index - 2] + state[index-1] == 38:
+            return True
         return False
 
     def checkRowsOf3(self, a, b, c, completeCheck):
@@ -150,5 +165,5 @@ class MagicHexagonProblem(search.Problem):
 
 nq1 = MagicHexagonProblem();
 start_time = time.time()
-print(search.breadth_first_tree_search(nq1).solution())
+print(search.depth_first_graph_search(nq1).solution())
 print("--- %s seconds ---" % (time.time() - start_time))
--- magichexagon/tmp.py
+++ magichexagon/tmp.py
@@ -16,19 +16,35 @@ class MagicHexagonProblem(search.Problem):
         # idx 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
         #     a,b,c,g,l,p,t,s,r,m,h, d, e, f, k, o, n, i, j
         # (18, 11, 9, 14, 15, 13, 10, 12, 16, 19, 3, 17, 1, 6, 8, 4, 2, 7, 5)
-
-        self.allNumbers = [19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        # [19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        self.allNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
         self.initial = tuple([0] * 19)
-        self.infinity = 1.0e400
 
     def actions(self, state):
-        # print(state)
         if state[0] == 0:
             return self.allNumbers
 
-        if self.check_partial_solution(state):
-            return [e for e in self.allNumbers if e not in state]
+        idx = state.index(0) - 1
+        if self.check_partial_solution(state, idx):
+            # see document "Equations for solving Magic hexagon problem.pdf"
+            # center value can be calculated if rows of 3 are calculated and correct
+            centerValue = 0
+            if 10 < idx < 17:
+                blue = (state[1] + state[3] + state[5] + state[7] + state[9] + state[11])
+                yellow = 114 - blue
+                centerValue = 19 - (yellow / 2)
+
+                # if invalid center value is found, this path will not be explored
+                if centerValue < 1:
+                    return []
+                elif centerValue in state:
+                    return []
+
+            # also this center value can not be put in any other index but the last, so its excluded from
+            # available numbers until last index is at hand.
+            return [e for e in self.allNumbers if e != centerValue and e not in state]
         else:
+            # paths that violate constraints, will not be explored further
             return []
 
     def result(self, state, action):
@@ -37,17 +53,32 @@ class MagicHexagonProblem(search.Problem):
         return tuple(localstate)
 
     def h(self, node):
-        s = 0
-        for e in node.state:
-            s += e;
-        return 190 - s;
+        state = node.state
+        s = sum(state)
+        h = state[10]
+
+        if h != 0:
+            # see document "Equations for solving Magic hexagon problem.pdf"
+            # 67 < p < 76, all other permutations for rows of 3 are penalised
+            p = state[0] + state[2] + state[4] + state[6] + state[8] + h
+            if p > 76:
+                p -= 76
+            elif p < 67:
+                p = 67 - p
+            else:
+                p = 0
+        else:
+            p = 0
+
+        return (190 - s) + (p * 150)
+
+    def h1(self, node):
+        return (190 - sum(node.state))
 
     def goal_test(self, state):
         return self.check_solution(state, True)
 
-    def check_partial_solution(self, state):
-        idx = state.index(0) - 1
-
+    def check_partial_solution(self, state, idx):
         if idx in (2, 4, 6, 8, 10):
             return state[idx - 2] + state[idx - 1] + state[idx] == 38
         elif idx == 11:
@@ -63,34 +94,34 @@ class MagicHexagonProblem(search.Problem):
         if goalCheck and state[-1] == 0:
             return False
 
-        a = state[0]
-        b = state[1]
-        c = state[2]
         g = state[3]
-        l = state[4]
-        p = state[5]
-        t = state[6]
-        s = state[7]
-        r = state[8]
-        m = state[9]
-        h = state[10]
         d = state[11]
         e = state[12]
         f = state[13]
-        k = state[14]
-        o = state[15]
-        n = state[16]
-        i = state[17]
-        j = state[18]
 
         if self.check_rows_of_4(d, e, f, g):
-            if self.check_rows_of_4(g, k, o, s):
-                if self.check_rows_of_4(b, f, k, p):
+            b = state[1]
+            p = state[5]
+            k = state[14]
+            if self.check_rows_of_4(b, f, k, p):
+                s = state[7]
+                o = state[15]
+                if self.check_rows_of_4(g, k, o, s):
+                    m = state[9]
+                    n = state[16]
                     if self.check_rows_of_4(m, n, o, p):
+                        i = state[17]
                         if self.check_rows_of_4(b, e, i, m):
                             if self.check_rows_of_4(d, i, n, s):
+                                c = state[2]
+                                r = state[8]
+                                j = state[18]
                                 if self.check_rows_of_5(c, f, j, n, r):
+                                    l = state[4]
+                                    h = state[10]
                                     if self.check_rows_of_5(h, i, j, k, l):
+                                        a = state[0]
+                                        t = state[6]
                                         if self.check_rows_of_5(a, e, j, o, t):
                                             return True
 
@@ -107,7 +138,65 @@ class MagicHexagonProblem(search.Problem):
         return a + b + c + d + e == 38
 
 
-nq1 = MagicHexagonProblem();
+class InstrumentedProblem(search.Problem):
+    """Delegates to a problem, and keeps statistics."""
+
+    def __init__(self, problem):
+        self.problem = problem
+        self.succs = self.goal_tests = self.states = self.heuristic = 0
+        self.found = None
+        self.start_time = time.time()
+
+    def actions(self, state):
+        self.succs += 1
+        return self.problem.actions(state)
+
+    def result(self, state, action):
+        self.states += 1
+        return self.problem.result(state, action)
+
+    def goal_test(self, state):
+        self.goal_tests += 1
+        result = self.problem.goal_test(state)
+        if result:
+            self.found = state
+        return result
+
+    def h(self, node):
+        self.heuristic += 1
+        return self.problem.h(node)
+
+    def path_cost(self, c, state1, action, state2):
+        return self.problem.path_cost(c, state1, action, state2)
+
+    def value(self, state):
+        return self.problem.value(state)
+
+    def __getattr__(self, attr):
+        return getattr(self.problem, attr)
+
+    def set_end_time(self, end_time):
+        self.end_time = end_time
+    def __repr__(self):
+        return '<%4d/%4d/%4d/%4d/%s/time: %s s>' % (self.succs, self.goal_tests,
+                                     self.states, self.heuristic, str(self.found)[:67], (self.end_time - self.start_time))
+
+def compare_searchers(problems, header,
+                      searchers=[search.depth_first_tree_search, search.depth_limited_search, search.depth_first_graph_search,
+                                 search.recursive_best_first_search, search.breadth_first_tree_search, search.iterative_deepening_search]):
+
+    def do(searcher, problem):
+        p = InstrumentedProblem(problem)
+        searcher(p)
+        p.set_end_time(time.time())
+        return p
+    table = [[search.name(s)] + [do(s, p) for p in problems] for s in searchers]
+    search.print_table(table, header)
+
+mhp = MagicHexagonProblem()
 start_time = time.time()
-print(search.greedy_best_first_graph_search(nq1, nq1.h).solution())
-print("--- %s seconds ---" % (time.time() - start_time))
+print("----- Start")
+#search.compare_searchers([mhp], ["Searcher","Result"])
+print(search.iterative_deepening_search(mhp).solution())
+#print(search.astar_search(mhp, mhp.h1).solution())
+print("--- Total %s seconds ---" % (time.time() - start_time))
